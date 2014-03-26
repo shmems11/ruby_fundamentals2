1da941c3591ffff4d4d27f4c040724f3d5eceb2e
shmems11 <shmems11@gmail.com>
2014-03-26 17:44:07 -0400

    added rice; made a method

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,15 @@
 grocery_list = ["bacon", "beef jerkey", "cheese", "pistachio icecream"]
+
+grocery_list << "rice" 
+
 grocery_list.each do |item| 
   puts "* " + item
-  end 
+  end
+
+def list(groceries)
+  groceries.each do |item| 
+    puts "* " + item
+  end
+end
+
+puts grocery_list.length 
