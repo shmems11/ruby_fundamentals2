11b19836c92b77b27fd7969cdec694b7e7cfa239
shmems11 <shmems11@gmail.com>
2014-03-27 00:29:51 -0400

    plus 5 percent

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,18 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+def num_students(students)
+	students.each do |k,v|
+		puts "#{k}: #{v} students"
+	end
+end
+num_students(students)
+
+students.store(:cohort4, 43)
+puts students.keys
+
+students.each do |k,v| 
+	v=(v * 0.05) + v
+	puts "#{k}: #{v} students"
+	end 
